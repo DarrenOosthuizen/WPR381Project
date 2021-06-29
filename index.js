8f2bfde4c2002ad195328b5dbf437fb208d39506
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-06-29 11:10:18 +0200

    Printed Extrati-nator Sign

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ let Amazing5 = '   \\__| |_| |_|  \\___|    \\__,_| |_| |_| |_|  \\__,_|  /___| 
 let Amazing6 = '                                                                         |___/';
 
 
+let Extract1 = '   _____  __  __  _____   ____       _       ____   _____   ___           _   _      _     _____    ___    ____  ';
+let Extract2 = '  | ____| \\ \\/ / |_   _| |  _ \\     / \\     / ___| |_   _| |_ _|         | \\ | |    / \\   |_   _|  / _ \\  |  _ \\ ';
+let Extract3 = '  |  _|    \\  /    | |   | |_) |   / - \\   | |       | |    | |   _____  |  \\| |   / - \\    | |   | | | | | |_) |';
+let Extract4 = '  | |___   /  \\    | |   |  _ <   / ___ \\  | |___    | |    | |  |_____| | |\\  |  / ___ \\   | |   | |_| | |  _ < ';
+let Extract5 = '  |_____| /_/\\_\\   |_|   |_| \\_\\ /_/   \\_\\  \\____|   |_|   |___|         |_| \\_| /_/   \\_\\  |_|    \\___/  |_| \\_\\';
+
 
 console.log('\x1b[33m%s\x1b[0m', Welcome1);
 console.log('\x1b[33m%s\x1b[0m', Welcome2);
@@ -28,5 +34,10 @@ console.log('\x1b[36m%s\x1b[0m', Amazing4);
 console.log('\x1b[36m%s\x1b[0m', Amazing5);
 console.log('\x1b[36m%s\x1b[0m', Amazing6);
 
+console.log('\x1b[31m%s\x1b[0m', Extract1);
+console.log('\x1b[31m%s\x1b[0m', Extract2);
+console.log('\x1b[31m%s\x1b[0m', Extract3);
+console.log('\x1b[31m%s\x1b[0m', Extract4);
+console.log('\x1b[31m%s\x1b[0m', Extract5);
 
 
