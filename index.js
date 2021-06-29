d9206efac2b32308ca1bd12e9445154d54c7c933
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-06-29 11:09:57 +0200

    Printed the Amazing Sign

--- index.js
+++ index.js
@@ -5,12 +5,28 @@ let Welcome4 =   '  \\ V  V /  |  __/ | | | (__  | (_) | | | | | | | |  __/    |
 let Welcome5 =   '   \\_/\\_/    \\___| |_|  \\___|  \\___/  |_| |_| |_|  \\___|     \\__|  \\___/';
 
 
+let Amazing1 = '   _     _                                                   _';
+let Amazing2 = '  | |_  | |__     ___      __ _   _ __ ___     __ _    ____ (_)  _ __     __ _ ';
+let Amazing3 = '  | __| | `_ \\   / _ \\    / _` | | `_ ` _ \\   / _` |  |_  / | | | `_ \\   / _` |';
+let Amazing4 = '  | |_  | | | | |  __/   | (_| | | | | | | | | (_| |   / /  | | | | | | | (_| |';
+let Amazing5 = '   \\__| |_| |_|  \\___|    \\__,_| |_| |_| |_|  \\__,_|  /___| |_| |_| |_|  \\__, |';
+let Amazing6 = '                                                                         |___/';
+
+
 
 console.log('\x1b[33m%s\x1b[0m', Welcome1);
 console.log('\x1b[33m%s\x1b[0m', Welcome2);
 console.log('\x1b[33m%s\x1b[0m', Welcome3);
 console.log('\x1b[33m%s\x1b[0m', Welcome4);
 console.log('\x1b[33m%s\x1b[0m', Welcome5);
+
+
+console.log('\x1b[36m%s\x1b[0m', Amazing1);
+console.log('\x1b[36m%s\x1b[0m', Amazing2);
+console.log('\x1b[36m%s\x1b[0m', Amazing3);
+console.log('\x1b[36m%s\x1b[0m', Amazing4);
+console.log('\x1b[36m%s\x1b[0m', Amazing5);
+console.log('\x1b[36m%s\x1b[0m', Amazing6);
 
 
 
