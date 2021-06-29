1b7bc4b6181f3fae81a73cfb0af3675ddaa005f8
DarrenOosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-06-29 11:09:35 +0200

    Printed Welcome Sign

---
+++ index.js
@@ -0,0 +1,16 @@
+let Welcome1 =   '__        __  	    _                                        _';
+let Welcome2 =   '\\ \\      / /  ___  | |   ___    ___    _ __ ___     ___     | |_    ___';
+let Welcome3 =   " \\ \\ /\\ / /  / _ \\ | |  / __|  / _ \\  | '_ ` _ \\   / _ \\    | __|  / _ \\";
+let Welcome4 =   '  \\ V  V /  |  __/ | | | (__  | (_) | | | | | | | |  __/    | |_  | (_) |';
+let Welcome5 =   '   \\_/\\_/    \\___| |_|  \\___|  \\___/  |_| |_| |_|  \\___|     \\__|  \\___/';
+
+
+
+console.log('\x1b[33m%s\x1b[0m', Welcome1);
+console.log('\x1b[33m%s\x1b[0m', Welcome2);
+console.log('\x1b[33m%s\x1b[0m', Welcome3);
+console.log('\x1b[33m%s\x1b[0m', Welcome4);
+console.log('\x1b[33m%s\x1b[0m', Welcome5);
+
+
+
