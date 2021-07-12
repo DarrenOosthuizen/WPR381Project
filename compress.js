2c7e22b0c0c53acd45428a2893cf52bd91b744e8
Darren Oosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-07-12 13:16:29 +0200

    Created Compress File

---
+++ compress.js
@@ -0,0 +1,13 @@
+//Function for looping through choosen directory to get all folders and subfolder
+import {ThroughDirectory,SortedDirectory, SortOrderFolder} from './directory.js'
+
+//Function to look for all folder in path
+ThroughDirectory("./foo")
+//Function to sort folders in order of tree
+SortOrderFolder()
+
+//SortedDirectory is array with filepath list in
+//get method for compresseing file to zip. FIND MORE FOR RAR and 7-ZIP 
+// ZIP RAR 7-ZIP
+
+//loop through list and compress each item in it
