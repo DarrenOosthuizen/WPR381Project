11747e1674ff24bc89eaf74e94035a5524393d56
Darren Oosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-07-12 10:24:06 +0200

    Imported Directory Method

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+//Function that will run to print the menu to the user
 import { PrintMenu } from "./menu.js"
+//Function for looping through choosen directory to get all folders and subfolder
+import {ThroughDirectory, Folders} from './directory.js'
 
+//Printing the Menu
 PrintMenu();
+
+
