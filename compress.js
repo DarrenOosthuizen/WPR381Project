811c9732d483af5011a18645aa215ab2786cd671
ClaudePretorius <clde.pretorius@gmail.com>
2021-07-12 22:06:54 +0200

    Imported module and coded compression
    Created function for compression in following types:
    Zip, RAR, Rar4, 7Zip
    
    Co-Authored-By: Plage25 <70269588+Plage25@users.noreply.github.com>

--- compress.js
+++ compress.js
@@ -1,13 +1,47 @@
 //Function for looping through choosen directory to get all folders and subfolder
-import {ThroughDirectory,SortedDirectory, SortOrderFolder} from './directory.js'
+import { ThroughDirectory, SortedDirectory, SortOrderFolder } from './directory.js'
+//Archive Import
+import { zip } from 'zip-a-folder';
 
 //Function to look for all folder in path
 ThroughDirectory("./foo")
 //Function to sort folders in order of tree
 SortOrderFolder()
 
-//SortedDirectory is array with filepath list in
-//get method for compresseing file to zip. FIND MORE FOR RAR and 7-ZIP 
-// ZIP RAR 7-ZIP
 
-//loop through list and compress each item in it
+//Function to Compress given folder directories to ZIP format
+function CompressZIP() {
+    SortedDirectory.forEach(async element => {
+        await zip(element, element + '.zip')
+    });
+}
+
+
+//Function to Compress given folder directories to RAR format
+function CompressRAR() {
+    SortedDirectory.forEach(async element => {
+
+        await zip(element, element + '.rar')
+    });
+}
+
+
+//Function to Compress given folder directories to 7zip format
+function Compress7ZIP() {
+    SortedDirectory.forEach(async element => {
+
+        await zip(element, element + '.7Z')
+    });
+}
+
+
+//Function to Compress given folder directories to RAR4 format
+function CompressRAR4() {
+    SortedDirectory.forEach(async element => {
+
+        await zip(element, element + '.rar4')
+    });
+}
+
+//Export the various types of compression methods for use in other .js files
+export { CompressZIP, Compress7ZIP, CompressRAR, CompressRAR4 }
