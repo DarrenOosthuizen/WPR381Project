b882552c7bac3d3a6da09ed89169133e203c32ae
Darren Oosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-07-14 08:01:50 +0200

    Removed Old Methods for Directory

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 //Function that will run to print the menu to the user
 import { PrintMenu } from "./menu.js"
-//Function for looping through choosen directory to get all folders and subfolder
-import {ThroughDirectory,SortedDirectory, SortOrderFolder} from './directory.js'
 
 //Printing the Menu
 PrintMenu();
 
-//Function to look for all folder in path
-ThroughDirectory("F:/Blizzard")
-//Function to sort folders in order of tree
-SortOrderFolder()
-//Array of Sorted Directories
-console.log(SortedDirectory)
 
 
