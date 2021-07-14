897474b485be53dacbcad06ac355c9855cd67ff2
Darren Oosthuizen <70310594+DarrenOosthuizen@users.noreply.github.com>
2021-07-14 17:08:07 +0200

    Main Page

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 //Function that will run to print the menu to the user
-import { PrintMenu } from "./menu.js"
+const { PrintMenu } = require("./menu");
 
 //Printing the Menu
 PrintMenu();
 
+const prompt = require("prompt-sync")();
 
-
+let running = true;
+while (running) {
+  let trueoption = false;
+  while (!trueoption) {
+    const Option = prompt("Enter CP to Compress Folders or EZ to Extract Zip:    ");
+    console.log(`You have choosen ${Option}`);
+    const Confirm = prompt("Please enter Y to confirm selection or N to rechoose")
+  }
+  console.log("bnrowse top location once in location type begin and press enter")
+}
